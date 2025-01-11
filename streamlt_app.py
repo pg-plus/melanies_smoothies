2360204b9992e9250895fcc2ce74f4798400479f
pg-plus <70606354+pg-plus@users.noreply.github.com>
2025-01-10 21:20:57 -0500

    Update streamlt_app.py
    adding food facts for every ingredients

--- streamlt_app.py
+++ streamlt_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 import requests
-# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -32,18 +31,14 @@ ingredients_list = st.multiselect(
     max_selections = 5
 )
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
     st.write(ingredients_string)
 
